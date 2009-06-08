893fae50275ac5ff423eef0e0394d71a46fcf8bb
clement <clement@ankara>
2009-06-09 00:49:10 +0700

    test view

--- wurfl/views.py
+++ wurfl/views.py
@@ -1,1 +1,7 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+from wurfl.decorators import device
+
+@device
+def test(request):
+    return render_to_response('wurfl/test.html', {}, context_instance=RequestContext(request))
